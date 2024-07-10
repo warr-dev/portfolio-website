2e1d3d1c8360a3ac179b97a874cf17f1ead41149
warrdev <warrdev08@gmail.com>
2024-07-10 22:13:30 +0800

    fix

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,8 +3,7 @@ function Hero() {
 
     return (
         <div class="h-screen w-full bg-slate-800 flex flex-col-reverse lg:flex-row text-white">
-            <div class="bg h-screen lg:w-1/2"></div>
-            {/* Text */}
+            <div class="bg h-full w-full lg:w-1/2"></div>
             <div class="hero-text flex flex-col w-full justify-center items-center lg:w-1/2 h-1/3 m-auto">
                 <div className="text-lg">Hi, I'm Warren</div>
                 <div className="text-5xl text-center">Full Stack Developer</div>
--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -5,9 +5,10 @@ function Socials() {
     return (
         <div class="absolute bottom-0 left-0 m-4 z-30">
             <div class="flex flex-col items-center gap-6 w-10">
-                <hr class="w-8 my-2 border-t-2 rotate-90" />
+                <hr class="w-8 my-2 border-t-3 rotate-90" />
                 <AiFillGithub size='2rem' color='white' />
                 <AiFillLinkedin size='2rem' color='white'  />
+                <hr class="w-4 my-2 border-t-3 rotate-90" />
             </div>
         </div>
     );
