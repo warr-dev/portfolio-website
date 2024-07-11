0d277c93cde9905a5ebf6da679cb5949b297f060
warrdev <warrdev08@gmail.com>
2024-07-11 12:15:50 +0800

    auto close nav on click

--- src/components/Drawer.jsx
+++ src/components/Drawer.jsx
@@ -14,13 +14,13 @@ function Drawer() {
                     setIsActive(!isActive())
                 }}>{!isActive() ? <FiMenu size={24} /> : <AiOutlineClose size={24} />}</button>
             </div>
-            <div className={`z-40 text-2xl md:text-3xl drawer transition-opacity absolute flex justify-center items-center w-full h-full bg-gray-300 ${!isActive() ? 'hidden opacity-0' : 'opacity-100'}`}>
-                <ul className='flex flex-col md:gap-4'>
-                    <A href='home' activeClass='font-bold text-slate-900' class="lg:hover:text-4xl hover:font-bold hover:text-3xl">Home</A>
-                    <A href='about' activeClass='font-bold text-slate-900' class="lg:hover:text-4xl hover:font-bold hover:text-3xl">About Me</A>
-                    <A href='projects' activeClass='font-bold text-slate-900' class="lg:hover:text-4xl hover:font-bold hover:text-3xl">Projects</A>
-                </ul>
-            </div>
+            <nav className={`z-40 text-2xl md:text-3xl drawer transition-opacity absolute flex justify-center items-center w-full h-full bg-gray-300 ${!isActive() ? 'hidden opacity-0' : 'opacity-100'}`}>
+                <div className='flex flex-col md:gap-4'>
+                    <A href='home' onclick={()=>setIsActive(false)} activeClass='font-bold text-slate-900' class="lg:hover:text-4xl hover:font-bold hover:text-3xl">Home</A>
+                    <A href='about' onclick={()=>setIsActive(false)} activeClass='font-bold text-slate-900' class="lg:hover:text-4xl hover:font-bold hover:text-3xl">About Me</A>
+                    <A href='projects' onclick={()=>setIsActive(false)} activeClass='font-bold text-slate-900' class="lg:hover:text-4xl hover:font-bold hover:text-3xl">Projects</A>
+                </div>
+            </nav>
         </>
     );
 }
